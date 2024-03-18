6884b424eef348a155c9bc559578f6493973e180
unknown <timidiodubo@gmail.com>
2024-03-18 12:01:25 +0100

    trial

--- starter-files/Classes-and-Objects/main.py
+++ starter-files/Classes-and-Objects/main.py
@@ -1,14 +1,17 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
     def __init__(self):
-        pass
+        # pass
+
+    def change_name(self, name):
+        self.name = name
 
 
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
+# Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+Bob = Student("Peter")
 # Expected methods
 Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+# Bob.change_age(34)
+# Bob.add_track("UI/UX")
+# Bob.get_score()
